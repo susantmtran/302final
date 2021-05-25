cb15a8053f9877d639bce90c8c15183d001f69f9
Grace Park <gracepark@Graces-MacBook-Pro-2.local>
2021-05-25 16:09:14 -0700

    update

--- app1/app.R
+++ app1/app.R
@@ -8,21 +8,21 @@
 #
 
 library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+macaroni <- readRDS("data/processed/macaroni.rds")  # word-by-word data
+
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Top 10 Most Frequently Used Words by Album"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            helpText("Pick Mac Miller's albums"),
+            selectInput("album", label = "Select album below", 
+                        choices = rownames(macaroni))
         ),
 
         # Show a plot of the generated distribution
@@ -37,11 +37,23 @@ server <- function(input, output) {
 
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        ## ggplot(aes(reorder(word, n), n, fill = reorder(word, -n))) +
+            ##ggplot(macaroni, aes_string(input$album)) 
+           ## geom_col(aes(word, n)) +
+            ##geom_text(aes(label = reorder(word, n)), 
+                     ## color = "white", size = 5, family = "Inconsolata",
+                     ## hjust = 1.5) +
+           ## scale_fill_brewer(palette = "Spectral") +
+           ## coord_flip() + # switch x and y
+            ##theme_classic() +
+          ##  theme(text = element_text(family = "Inconsolata"),
+                 ## axis.text.y = element_blank(),
+                 ## axis.title = element_blank(),
+                ##  legend.position = "none",
+               ##   plot.title = element_text(family = "Royal Acid", size = 10, hjust = 0.5),
+                ##  plot.subtitle = element_text(hjust = 0.5))
+        barplot(macaroni[,input$album],
+                main=input$album)
     })
 }
 
