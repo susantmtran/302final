e1b513497f16480a98b3f006d4c35a220d4534d7
Grace Park <gracepark@Graces-MacBook-Pro-2.local>
2021-05-25 20:45:10 -0700

    updated

--- love_section_vis.R
+++ love_section_vis.R
@@ -1,14 +1,20 @@
+# focusing on love aspect of mac miller songs
+
 library(tidyverse)
 library(tidytext)
-library(stringr)
-library(dplyr)
+library(showtext)
+library(extrafont)
+library(ggthemes)
+library(RColorBrewer)
+library(ggrepel)
+library(patchwork)
 
 macaroni <- readRDS("data/processed/macaroni.rds")  # word-by-word data
 
 macaroni_love <- macaroni %>%
   filter(word == "love" | word == "baby" | word == "heart" | word == "beautiful" | word == "kiss" | word == "soulmate") 
 
-macaroni_love %>% 
+love_words <- macaroni_love %>% 
   count(word, sort = TRUE) %>% 
   top_n(10) %>% 
   ggplot(aes(reorder(word, n), n, fill = reorder(word, -n))) +
@@ -18,14 +24,16 @@ macaroni_love %>%
             vjust = -0.6) +
   scale_fill_brewer(palette = "RdPu") +
   theme_classic() +
-  ggtitle("Top 10 Most Frequently Used Words",
+  ggtitle("Top 10 Most Frequently Used Words", 
           subtitle = "Mac Miller Discography 2010–2020 ") +
   theme(text = element_text(family = "Inconsolata"),
         axis.text.y = element_blank(),
         axis.title = element_blank(),
         legend.position = "none",
-        plot.title = element_text(family = "Royal Acid", size = 10, hjust = 0.5),
+        plot.title = element_text(family = "Royal Acid", size = 15, hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
+
+ggsave("plots/love_common_words.png")
 
 
 
