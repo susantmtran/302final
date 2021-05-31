6789c2aff85a2018010e36757c96768bd7c7f953
Grace Park <gracepark@Graces-MacBook-Pro-2.local>
2021-05-31 10:58:39 -0700

    updated

--- app1/app.R
+++ app1/app.R
@@ -20,14 +20,18 @@ library(wordcloud2)
 library(markdown)
 library(patchwork)
 
-macaroni <- readRDS("data/processed/macaroni.rds")  # word-by-word data
+# word-by-word data
+macaroni <- readRDS("data/processed/macaroni.rds") 
+
+# first tab initial steps
 love_words <- sort(unique(str_subset(macaroni$word, "^lov")))
+
 love_counts <- macaroni %>%
   group_by(album, song) %>%
   summarise(love_count = sum(word %in% love_words))
+
 top_love <- love_counts %>%
   top_n(5, love_count)
-
 
 album <- list("The Divine Feminine" = "the divine feminine",
               "Faces" = "faces", 
@@ -35,7 +39,20 @@ album <- list("The Divine Feminine" = "the divine feminine",
               "Swimming" = "swimming",
               "Circles" = "circles")
 
-ui <- fluidPage(
+# second tab initial steps
+wordcloud_data <- macaroni %>%
+  anti_join(stop_words) %>%
+  group_by(word) %>%
+  filter(n() >= 10)
+
+wordcloud_data <- wordcloud_data[!(wordcloud_data$word %in% c("yeah", "wanna", 
+                                                              "uh","told", "til", "da", 
+                                                              "i'ma", "em")),]
+
+word_count <- count(wordcloud_data, word) 
+
+
+ui <- navbarPage(
   tags$head(
     tags$style("label{font-family: Inconsolata;}")
   ),
@@ -46,7 +63,7 @@ ui <- fluidPage(
            # Sidebar with a slider input for number of bins 
            sidebarLayout(position = "right",
                          sidebarPanel(
-                           img(src = "mac.jpeg", width = "80%", style="display: block; margin-left: auto; margin-right: auto;"), br(),
+                           img(src = "mac.jpeg", width = "60%", style="display: block; margin-left: auto; margin-right: auto;"), br(),
                            style = "background: #ffd1dc",
                            helpText("Pick one of Mac Miller's albums to see how many times 'Love' 
                      (or some variation of 'Love') is in his songs."),
@@ -60,9 +77,21 @@ ui <- fluidPage(
                            plotOutput("lovecountPlot")
                          )
            )
-  )
+  ),
+  tabPanel("Lyrical Word Cloud",
+           titlePanel("Mac's Word Cloud"),
+           sidebarLayout(position = "right",
+                         sidebarPanel(style = "background:#ffcba4",
+                                      img(src = "orangemac.png", width = "80%", style="display: block; margin-left: auto; margin-right: auto;"), br(),
+                                      helpText("Hover over the words to see how many times each non-stop
+                                       word appeared in Mac Miller's song collection."),
+                                      br(),
+                                      "Data used from ___source"
+                         ),
+                         mainPanel(
+                           wordcloud2Output("wordcloud", width = "100%", height = "565px")))
   
-)
+))
 
 
 # Define server logic required to draw a histogram
@@ -88,6 +117,11 @@ server <- function(input, output) {
             axis.text.x = element_text(),
             plot.title = element_text(family = "Royal Acid", size = 10, hjust = 0.5),
             plot.subtitle = element_text(hjust = 0.5))})
+  
+  output$wordcloud <- renderWordcloud2({
+    wordcloud2(word_count, color = rep_len(
+      c("orange", "#d0c816", "#f2905a", "#ff8668"), 
+      nrow(word_count)), shape = "triangle-forward")})
   
 }
 
