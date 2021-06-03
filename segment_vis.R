58ed59d65e72cd5be9feb61337d20ff11f43dbe1
Grace Park <gracepark@Graces-MacBook-Pro-2.local>
2021-06-02 20:54:44 -0700

    updated

---
+++ segment_vis.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+macaroni <- readRDS("data/processed/macaroni.rds") # word-by-word data
+
+# chose Faces album because it has the most amount of songs.
+macaroni <- macaroni %>%
+  filter(album == "faces") %>%
+  count(song)
+
+ggplot(macaroni,
+       aes(x = n,
+           y = reorder(song, n))) +
+  geom_point(color = "#ACDF87") +
+  geom_segment(aes(x = 10, 
+                   xend = n,
+                   y = reorder(song, n),
+                   yend = reorder(song, n)),
+               color = "#96A563") +
+  labs(x = "Number of Words",
+       y = "",
+       title = "FACES Album",
+       subtitle = "Number of words in each song in 'Faces'") +
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ggsave("plots/faces_album.png")
